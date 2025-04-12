0df5e49298a6cf0ba5380c70ffec0f25d4db1288
Moritz <moritz.glowatzki@gmail.com>
2025-04-12 22:26:14 +0200

    add trajectory visualization

---
+++ trajectory_visualization.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+from data_IO import *
+
+# Constants
+RAW_SMALL_DATA_PATH = "./data/cityA-dataset-small.csv"
+DAY = 0
+NUM_FRAMES = 48
+
+# Load data
+df = load_csv_file(RAW_SMALL_DATA_PATH)
+df_day = df[df['d'] == DAY]
+df_day_size = len(df_day)
+
+# Set up figure and axes
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.set_xlabel('X', fontsize=12)
+ax.set_ylabel('Y', fontsize=12)
+ax.set_title(label=f'Trajectory uid=0 on day {DAY}', fontsize=21, fontweight='bold')
+
+# Grid and ticks
+min_x, max_x = df_day['x'].min() - 2, df_day['x'].max() + 2
+min_y, max_y = df_day['y'].min() - 2, df_day['y'].max() + 2
+
+ax.set_xticks(np.arange(min_x, max_x + 1, 1))
+ax.set_xticks(np.arange(-0.5, 200, 1), minor=True)
+ax.set_yticks(np.arange(min_y, max_y + 1, 1))
+ax.set_yticks(np.arange(-0.5, 200, 1), minor=True)
+ax.grid(which="minor")
+ax.tick_params(which="minor", size=0)
+
+ax.set_xlim(min_x - 0.5, max_x + 0.5)
+ax.set_ylim(min_y - 0.5, max_y + 0.5)
+ax.set_aspect("equal")
+
+# Plot elements
+line, = ax.plot([], [], alpha=0.7)
+scat_past = ax.scatter([], [], zorder=2)
+scat_current = ax.scatter([], [], color="red", zorder=3)
+quiver = ax.quiver(0, 0, 0, 0, angles="xy", scale_units="xy", scale=1, width=0.01)
+time_text = ax.annotate(
+    '',
+    xy=(0, 1),
+    xycoords='axes fraction',
+    xytext=(10, -10),
+    textcoords='offset points',
+    fontsize=14,
+    ha='left',
+    va='top',
+    bbox=dict(facecolor='white', alpha=0.8)
+)
+
+def animate(i):
+    tmp = df_day[df_day['t'] <= i]
+    current_frame_data = df_day[df_day['t'] == i]
+
+    if not current_frame_data.empty:
+        # Get past and current points
+        if len(tmp) > 1:
+            past_points = tmp.iloc[:-1][['x', 'y']].values
+            last_point = tmp.iloc[-1][['x', 'y']].values
+        else:
+            past_points = np.empty((0, 2))
+            last_point = tmp.iloc[-1][['x', 'y']].values
+
+        # Update scatter plots
+        scat_past.set_offsets(past_points)
+        scat_current.set_offsets([last_point])
+
+        # Update line
+        line.set_data(tmp['x'], tmp['y'])
+
+        # Update quiver
+        if len(tmp) > 1:
+            x_last, y_last = tmp.iloc[-1][['x', 'y']]
+            x_prev, y_prev = tmp.iloc[-2][['x', 'y']]
+            dx, dy = x_last - x_prev, y_last - y_prev
+
+            quiver.set_offsets([x_prev, y_prev])
+            quiver.set_UVC(dx, dy)
+        else:
+            quiver.set_offsets([0, 0])
+            quiver.set_UVC(0, 0)
+
+        # Update time label
+        time_text.set_text(f'Time: {i}')
+    else:
+        # No update, hide quiver and current point
+        quiver.set_offsets([0, 0])
+        quiver.set_UVC(0, 0)
+        time_text.set_text(f'Time: {i} (no data)')
+
+    return scat_past, scat_current, line, quiver, time_text
+
+# Create and save animation
+anim = FuncAnimation(fig, animate, frames=NUM_FRAMES + 1, interval=500, repeat=False)
+anim.save(f'trajectory_0_day{DAY}.gif', writer='Pillow')
