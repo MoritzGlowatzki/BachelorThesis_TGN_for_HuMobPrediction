8b2a49f613b9d8ed1d0cf0f1c51b20dc73b1bec6
Moritz <moritz.glowatzki@gmail.com>
2025-04-14 23:19:49 +0200

    corrections in data_preprocessing.py

--- III_data_preprocessing.py
+++ III_data_preprocessing.py
@@ -1,12 +1,6 @@
-from math import cos
-from math import sin
-
 import numpy as np
 
 from I_data_IO import *
-
-RAW_DATA_PATH = "./data/cityA-dataset-small.csv"
-data = load_csv_file(RAW_DATA_PATH)
 
 
 # -------- Estimations, Calculations, Helper Functions -------- #
@@ -18,7 +12,6 @@ def estimate_home_location(df):
             [["x", "y"]]
             .agg(lambda location: location.mode().iloc[0]))
 
-
 def estimate_work_location(df):
     # estimated work location = most frequent location during work hours (9 am to 5 pm)
     return (df
@@ -26,10 +19,8 @@ def estimate_work_location(df):
             [["x", "y"]]
             .agg(lambda location: location.mode().iloc[0]))
 
-
 def calculate_average_number_of_movements_per_day(df, uid):
     return df[df["uid" == uid]].size().mean()
-
 
 def calculate_average_travel_distance_per_day(df, uid):
     if "distance_to_last_position" in df.columns:
@@ -44,29 +35,29 @@ def calculate_euclidean_distance(x_1, y_1, x_2, y_2):
 
 # -------- Data Preprocessing Pipeline -------- #
 
-def data_preprocessing():
+def data_preprocessing(data):
     # sin-cos-transformations of "day"
-    data["d_sin"] = sin(data["d"] / 7)
-    data["d_cos"] = cos(data["d"] / 7)
+    data["d_sin"] = np.sin(data["d"] / 7)
+    data["d_cos"] = np.cos(data["d"] / 7)
 
     # sin-cos-transformations of "time"
-    data["t_sin"] = sin(data["t"] / 48)
-    data["t_cos"] = cos(data["t"] / 48)
+    data["t_sin"] = np.sin(data["t"] / 48)
+    data["t_cos"] = np.cos(data["t"] / 48)
 
     # weekday
     data["weekday"] = data["d"] % 7
 
     # weekend
-    data["weekend"] = (data["weekday"] == 0 | data["weekday"] == 6)
+    data["weekend"] = (data["weekday"] == 0) | (data["weekday"] == 6)
 
     # daytime: 1, nighttime: 0 (= time between 7am and 7pm)
-    data["daytime"] = (14 <= data["t"] < 38)
+    data["daytime"] = (14 <= data["t"]) & (data["t"] < 38)
 
     # am: 1, pm: 0
-    data["am"] = (0 <= data["t"] < 24)
+    data["am"] = (0 <= data["t"]) & (data["t"] < 24)
 
     # timedelta
-    data["timedelta"] -= data["timedelta"].shift(1).fillna(0)
+    data["timedelta"] = data["t"] - data["t"].shift(1).fillna(0)
 
     # euclidean distance to last position
     data["distance_to_last_position"] = calculate_euclidean_distance(data["x"], data["y"], data["x"].shift(1).fillna(0),
@@ -84,11 +75,19 @@ def data_preprocessing():
     data["distance_from_work"] = calculate_euclidean_distance(data["x"], data["y"], work["x"], work["y"])
 
     # home: 0, work: 1, else: 2
-    data["currently_at"] = (
-        0 if ((data["x"] == home["x"]) & (data["y"] == home["y"]))
-        else 1 if ((data["x"] == work["x"]) & (data["y"] == work["y"]))
-        else 2
-    )
+    # Initialize with default value and apply masks
+    data["currently_at"] = 2
+    data.loc[(data["x"] == home["x"]) & (data["y"] == home["y"]), "currently_at"] = 0
+    data.loc[(data["x"] == work["x"]) & (data["y"] == work["y"]), "currently_at"] = 1
+
+    return data
+
+
+if __name__ == "__main__":
+    RAW_DATA_PATH = "./data/cityA-dataset-small.csv"
+    data = load_csv_file(RAW_DATA_PATH)
+
+    preprocessed_data = data_preprocessing(data)
 
     PREPROCESSED_DATA_PATH = "./data/cityA-dataset-small-preprocessed.csv"
     store_csv_file(PREPROCESSED_DATA_PATH, data)
