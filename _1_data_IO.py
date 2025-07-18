25bde13d1de98e386af6950890fb4726ed5158e5
Moritz <moritz.glowatzki@gmail.com>
2025-07-18 17:38:27 +0200

    update _1_data_IO.py

--- _1_data_IO.py
+++ _1_data_IO.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-RAW_DATA_PATH = "./data/original/cityA-dataset.csv"
-RAW_SMALL_DATA_PATH = "./data/original/cityA-dataset-small.csv"
-
 def load_csv_file(path):
     return pd.read_csv(path)
 
@@ -12,7 +9,13 @@ def store_csv_file(path, content):
 
 if __name__ == "__main__":
     # read the CSV file
+    RAW_DATA_PATH = "./data/dataset_humob_2024/cityD_challengedata.csv"
     data = load_csv_file(RAW_DATA_PATH)
 
-    # Filter only rows where uid == 0
-    store_csv_file(RAW_SMALL_DATA_PATH, data[data["uid"] <= 1])
+    # filter only rows where uid == 0
+    RAW_SMALL_DATA_PATH = "./data/dataset_humob_2024/full_city_data/cityA-dataset-small.csv"
+    store_csv_file(RAW_SMALL_DATA_PATH, data[data["uid"] == 0])
+
+    # filter only rows where x and y == 999
+    RAW_PREDICT_DATA_PATH = "./data/dataset_humob_2024/cityD_to_predict.csv"
+    store_csv_file(RAW_PREDICT_DATA_PATH, data[(data["x"] == 999) & (data["y"] == 999)])
