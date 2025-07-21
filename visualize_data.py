c7e729828f13109afc49ba05c3edd7093fb9d8fc
Moritz <moritz.glowatzki@gmail.com>
2025-07-21 21:20:10 +0200

    update plot_daily_data_records_per_city() and adapt text sizes

--- visualize_data.py
+++ visualize_data.py
@@ -18,37 +18,34 @@ DATA_PATHS = {
     "B": "./data/original/cityB-dataset.csv",
     "C": "./data/original/cityC-dataset.csv",
     "D": "./data/original/cityD-dataset.csv",
-    "A-small": "./data/original/cityA-dataset-small.csv",
 }
 
 def plot_daily_data_records_per_city():
     labels_map = ["City A", "City B", "City C", "City D"]
     colors_map = ["C0", "C1", "C2", "C3"]
 
-    plt.figure(figsize=(15, 5))
+    plt.figure(figsize=(15, 10))
+
+    # -------- Plot each dataset -------- #
+    for path, label, color in tqdm(zip(DATA_PATHS.values(), labels_map, colors_map), total=len(DATA_PATHS)):
+        data = load_csv_file(path)
+        counts = data.groupby("d").size()
+        counts.plot(style=".-", label=label, color=color, markersize=12)
 
     # -------- Highlight weekends with gray bars and tick_labels -------- #
-    x_labels = []
     for d in range(75):
         if d % 7 == 0 or d % 7 == 6:
             plt.axvspan(d - 0.5, d + 0.5, color="lightgray", alpha=1)
-            x_labels.append(str(d))
-        else:
-            x_labels.append("")
-    plt.xticks(ticks=range(75), labels=x_labels, fontsize=8, rotation=0)
-
-    # -------- Plot each dataset -------- #
-    for path, label, color in tqdm(zip(DATA_PATHS, labels_map, colors_map), total=len(DATA_PATHS)):
-        data = load_csv_file(path)
-        counts = data.groupby("d").size()
-        counts.plot(style=".-", label=label, color=color)
+    plt.xticks(ticks=range(75), labels=[str(d) if d % 2 == 0 else "" for d in range(75)], fontsize=14)
 
     # -------- Final plot adaptations -------- #
-    plt.title("Daily Data Records per City")
-    plt.xlabel("Day")
-    plt.ylabel("Number of records (log scale)")
+    plt.title("Daily Data Records per City", fontsize=20, fontweight="bold")
+    plt.xlabel("Day", fontsize=16)
+    plt.xlim(-2, 76)
+    plt.ylabel("Number of records (log scale)", fontsize=16)
     plt.yscale("log")
-    plt.legend()
+    plt.yticks(fontsize=14)
+    plt.legend(loc='upper right', fontsize=16)
     plt.tight_layout()
 
     # -------- Show and save plot -------- #
@@ -56,9 +53,44 @@ def plot_daily_data_records_per_city():
     plt.show()
 
 
-def plot_gravitational_centres_for_single_user(dataset_index, uid):
+def plot_avg_data_records_per_hour_per_city():
+    labels_map = ["City A", "City B", "City C", "City D"]
+    colors_map = ["C0", "C1", "C2", "C3"]
+
+    plt.figure(figsize=(15, 10))
+    plt.axvline(x=14, color='lightgray', linewidth=3)
+    plt.axvline(x=38, color='lightgray', linewidth=3)
+
+    # -------- Plot each dataset -------- #
+    for path, label, color in tqdm(zip(DATA_PATHS.values(), labels_map, colors_map), total=len(DATA_PATHS)):
+        data = load_csv_file(path)
+        avg_per_hour = data.groupby(["t", "d"]).size().groupby("t").mean()
+        avg_per_hour.plot(style=".-", label=label, color=color, markersize=12)
+
+    # -------- Create hourly tick_labels and vertical lines to separate day&night -------- #
+    plt.xticks(ticks=range(48), labels=[f"{int(h / 2)}:00" if h % 2 == 0 else "" for h in range(48)], fontsize=14,
+               rotation=45)
+    plt.axvspan(-2, 14, color="lightgray", alpha=1)
+    plt.axvspan(38, 50, color="lightgray", alpha=1)
+
+    # -------- Final plot adaptations -------- #
+    plt.title("Average Number of Moving Users per Half-Hour Interval per City", fontsize=20, fontweight="bold")
+    plt.xlabel("Time of Day", fontsize=16)
+    plt.xlim(-2, 49)
+    plt.ylabel("Average number of users (log scale)", fontsize=16)
+    plt.yscale("log")
+    plt.yticks(fontsize=14)
+    plt.legend(loc='upper right', fontsize=16)
+    plt.tight_layout()
+
+    # -------- Show and save plot -------- #
+    plt.savefig("./output/histogram_hourly_data_records_per_day.png")
+    plt.show()
+
+
+def plot_gravitational_centres_for_single_user(dataset_index, uid, city_id):
     original_data = load_csv_file(DATA_PATHS[dataset_index])
-    data = original_data[(original_data["uid"] == uid)][["x", "y", "t"]]
+    data = original_data[(original_data["uid"] == uid)][["x", "y", "t", "d"]]
 
     combinations = [(True, True), (True, False), (False, True)]
     titles = [
@@ -79,7 +111,7 @@ def plot_gravitational_centres_for_single_user(dataset_index, uid):
     norm = Normalize(vmin=global_vmin, vmax=global_vmax)
 
     # Create a figure and axes
-    fig, axes = plt.subplots(1, 3, figsize=(24, 8), constrained_layout=True)
+    fig, axes = plt.subplots(1, 3, figsize=(24, 6), constrained_layout=True)
 
     # -------- Create subplots -------- #
     for idx, (show_home, show_work) in enumerate(combinations):
@@ -87,16 +119,36 @@ def plot_gravitational_centres_for_single_user(dataset_index, uid):
         ax.set_facecolor("#440154")
 
         filtered_data = data.copy()
+        filtered_data["weekday"] = filtered_data["d"] % 7
+
         if show_home and not show_work:
-            filtered_data = filtered_data[((0 <= filtered_data["t"]) & (filtered_data["t"] <= 16))
-                                          | ((40 <= filtered_data["t"]) & (filtered_data["t"] < 48))]
+            # Home: early morning + evening on weekdays, entire day on weekends
+            filtered_data = filtered_data[
+                (
+                        ((1 <= filtered_data["weekday"]) & (filtered_data["weekday"] <= 5)) &  # Weekdays: 1–5
+                        (
+                                ((0 <= filtered_data["t"]) & (filtered_data["t"] <= 16)) |  # 00:00–08:00
+                                ((40 <= filtered_data["t"]) & (filtered_data["t"] < 48))  # 20:00–24:00
+                        )
+                ) |
+                ((filtered_data["weekday"] == 0) | (filtered_data["weekday"] == 6))
+                # Weekend: Sunday (0) & Saturday (6)
+                ]
+
         elif not show_home and show_work:
-            filtered_data = filtered_data[(18 <= filtered_data["t"]) & (filtered_data["t"] < 38)]
+            # Work: 9–5 only on weekdays
+            filtered_data = filtered_data[
+                ((1 <= filtered_data["weekday"]) & (filtered_data["weekday"] <= 5)) &  # Weekdays: 1–5
+                ((18 <= filtered_data["t"]) & (filtered_data["t"] < 34))  # 09:00–17:00
+                ]
 
         # Plot the histplot with the color normalization
         sns.histplot(data=filtered_data, x="x", y="y", cmap="viridis", thresh=0, bins=100, ax=ax, cbar=True,
                      cbar_kws={"shrink": 0.625, "label": "Absolute Density of Coordinates"},
                      vmin=global_vmin, vmax=global_vmax)
+        cbar = ax.collections[0].colorbar
+        cbar.set_label("Absolute Density of Coordinates", fontsize=16)
+        cbar.ax.tick_params(labelsize=12)
 
         # Plot the kdeplot with the same color normalization
         sns.kdeplot(data=filtered_data, x="x", y="y", fill=True, cmap="viridis", thresh=0, bw_adjust=0.5, ax=ax,
@@ -126,12 +178,12 @@ def plot_gravitational_centres_for_single_user(dataset_index, uid):
         # -------- Final plot adaptations -------- #
         ax.set_xlim(x_min, x_max)
         ax.set_ylim(y_min, y_max)
-        ax.set_title(titles[idx], fontsize=16, fontweight="bold")
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
+        ax.set_title(titles[idx], fontsize=18, fontweight="bold")
+        ax.set_xlabel("X", fontsize=16)
+        ax.set_ylabel("Y", fontsize=16)
         ax.set_aspect("equal")
 
-    plt.suptitle(f"Gravitational Centres for uid={uid}", fontsize=24, fontweight="bold")
+    plt.suptitle(f"Gravitational Centres for uid={uid} in city {city_id}    ", fontsize=24, fontweight="bold")
 
     # -------- Save and show plot -------- #
     plt.savefig(f"./output/gravitational_centres_uid_{uid}.png", dpi=300, bbox_inches="tight", pad_inches=0.5)
@@ -205,12 +257,14 @@ def visualize_multiday_user_trajectory(dataset_index, uid):
 
 if __name__ == "__main__":
     # plot_daily_data_records_per_city()
+    # plot_avg_data_records_per_hour_per_city()
     # for i in tqdm(range(0, 3), total=3):
     #     plot_gravitational_centres_for_single_user("A", i)
+    plot_gravitational_centres_for_single_user("A", 0, "A")
     # plot_gravitational_centres_for_single_user("A", 14959)
     # plot_gravitational_centres_for_single_user("A", 26176)
     # plot_gravitational_centres_for_single_user("A", 60369)
-    plot_gravitational_centres_all_cities()
+    # plot_gravitational_centres_all_cities()
     # for i in tqdm(range(0, 3), total=3):
         # visualize_single_trajectory("A-small", 0, i, "real", True)
     # visualize_single_trajectory("D", 0, i, "real", False)
