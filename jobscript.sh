244f4121c94ac3fca8937c95394eb6cd71aa9e12
MoritzGlowatzki <moritz.glowatzki@gmail.com>
2025-08-18 09:51:46 +0200

    jobscript complete

--- jobscript.sh
+++ jobscript.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Install necessary packages (GeoBLEU and DTW metric)
-# pip install git+https://github.com/yahoojapan/geobleu.git
+pip install git+https://github.com/yahoojapan/geobleu.git
 
 # Preprocess data
-# python -u _2a_data_preprocessing.py --city="D" --process="trajectory,user,location" 
+python -u _2a_data_preprocessing.py --city="D" --process="trajectory,user,location" 
 
 # Create TemporalDataset
-python -u _2b_dataset.py --city="D"
+python -u _2b_dataset.py --city="D" --interpol=True --small=True --feats=True
 
 # Train TGN model
-python -u _3_model_training.py --city="D" --epochs=50 --save_path="./model_training_runs/best_model_D_50.pt"
+python -u _3_model_training.py --city="D" --epochs=50 --interpol=True --small=True --feats=True --neg_sampling_ratio=50
+
+# Predict with TGN model
+python -u _4_predict.py --city="D" --small=True --interpol=True --feats=True --model="last_model_50_on_D_True_True"
+
+# Evaluate predictions
+python -u _5_evaluate.py --city="D" --model="last_model_50_on_D_True_True"
